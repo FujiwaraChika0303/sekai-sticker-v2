d1041a743ce476c292feac2b13f0ee55b2ff43d2
Yukinoshita <53087601+FujiwaraChika0303@users.noreply.github.com>
2025-05-22 12:15:07 -0400

    774

--- src/components/about/content/GeneralInformations.tsx
+++ src/components/about/content/GeneralInformations.tsx
@@ -5,24 +5,7 @@ import Markdown from 'markdown-to-jsx'
 const contentOne = `
 *(English Only)*
 
-# 🐙 Github
-https://github.com/r48n34/sekai-sticker-v2
 
-   
-# 🙏 Special Thanks
-This version is base on: 
-
-https://github.com/atnightcord/sekai-stickers
-
-https://github.com/TheOriginalAyaka/sekai-stickers
-
-
-## 👏 Contributes from:
-- @akiyamamizuki (Focked version) 
-
-- @ayaka (Original) 
-
-`
 
 function GeneralInformations() {
     return null; // 这样组件就不会显示任何内容
